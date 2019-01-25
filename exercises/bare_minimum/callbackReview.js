9450b370e10cafb7e69184c0ce3e8db7e4a31e50
chriztso <christophertso2017@u.northwestern.edu>
2019-01-24 19:55:17 -0800

    Complete callbackReview

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -1,18 +1,64 @@
 /**
  * Implement these functions following the node style callback pattern
  */
-
 var fs = require('fs');
 var request = require('request');
+const https = require('https');
+var Promise = require('bluebird')
+
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+// var pluckFirstLineFromFile = function (filePath, callback) {
+//   fs.readFile(filePath, (err, data) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, data.toString().split("\n")[0]);
+//     }
+//   })
+// };
+
+// const myFirstPromise = new Promise((resolve, reject) => {
+//   // do something asynchronous which eventually calls either:
+//   //
+//   //   resolve(someValue); // fulfilled
+//   // or
+//   //   reject("failure reason"); // rejected
+// });
+var pluckFirstLineFromFile = function (filePath, callback) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+
+    })
+      resolve(null, data.toString().split("\n")[0]);
+      reject(err);
+  });
+}
+
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data.toString().split("\n")[0]);
+    }
+  })
 };
 
+var error = {message: 'Invalid URI'};
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  https.get(url, (resp) => {
+    const { statusCode } = resp;
+    callback(null, statusCode);
+    resp.on('end', () => {
+    });
+  }).on("error", 
+  (err) => {
+    err['message'] = "Invalid URI";
+    console.log(err.message);
+    callback(err);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
