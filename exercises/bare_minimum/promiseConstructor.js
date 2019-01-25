93a422a1c62e2e4ccf8532a8385eef1ca648b107
chriztso <christophertso2017@u.northwestern.edu>
2019-01-24 19:55:55 -0800

    Complete promisification and promise constructors

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -6,16 +6,51 @@
 
 var fs = require('fs');
 var request = require('request');
+const https = require('https');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString().split('\n')[0]);
+      }
+    });
+  });
 };
 
+
+// This function should retrieve the status code of a GET request to `url`
+var getStatusCode = function (url, callback) {
+  https.get(url, (resp) => {
+    const { statusCode } = resp;
+    callback(null, statusCode);
+    resp.on('end', () => {
+    });
+  }).on("error", 
+  (err) => {
+    err['message'] = "Invalid URI";
+    console.log(err.message);
+    callback(err);
+  });
+};
+
+var error = {message: 'Invalid URI'};
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    https.get(url, (resp) => {
+      const { statusCode } = resp; 
+      resolve(statusCode); 
+      resp.on('end', () => {});
+    }).on("error", (err) => {
+      err['message'] = "Invalid URI";
+      reject(err);
+    });
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
