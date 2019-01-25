e930da70bd4f03835bfd44997e64f48f1397a3b3
chriztso <christophertso2017@u.northwestern.edu>
2019-01-24 21:02:24 -0800

    Worked on basic chaining

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,17 +8,17 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
+var fs = Promise.promisify(require('fs'));
 var Promise = require('bluebird');
 var crypto = require('crypto');
 var request = require('request');
-var pluk = require('./promiseConstructor');
-var userId = require('./promisification.js');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  return pluk.pluckFirstLineFromFileAsync(readFilePath);
+  return fs.readFileAsync((readFilePath, 'utf8') => {
+        
+  })
 };
 
 // Export these functions so we can test them
